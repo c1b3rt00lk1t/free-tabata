b82813b29ff3fdbe04a06d19af52be775e896904
c1b3rt00lk1t <c1b3rt00lk1t@protonmail.com>
2022-05-17 21:45:34 +0200

    set state functions commented to avoid warnings in the deployment

--- src/context/FreeTabataContext.js
+++ src/context/FreeTabataContext.js
@@ -1,15 +1,25 @@
-import { createContext, useState, useEffect } from "react";
+import { createContext, useState } from "react";
 
 const FreeTabataContext = createContext();
 
 export const FreeTabataProvider = ({ children }) => {
-  const [prepare, setPrepare] = useState(10);
-  const [work, setWork] = useState(20);
-  const [rest, setRest] = useState(10);
-  const [tabatas, setTabatas] = useState(1);
-  const [cycles, setCycles] = useState(8);
-  const [generalMode, setGeneralMode] = useState(0); // it could be stopped (0), on-going (1), paused (2)
-  const [cycleMode, setCycleMode] = useState("none"); // it could be none, prepare, work, rest
+  // const [prepare, setPrepare] = useState(10);
+  // const [work, setWork] = useState(20);
+  // const [rest, setRest] = useState(10);
+  // const [tabatas, setTabatas] = useState(1);
+  // const [cycles, setCycles] = useState(8);
+  // const [generalMode, setGeneralMode] = useState(0); // it could be stopped (0), on-going (1), paused (2)
+  // const [cycleMode, setCycleMode] = useState("none"); // it could be none, prepare, work, rest
+
+
+  const [prepare] = useState(10);
+  const [work] = useState(20);
+  const [rest] = useState(10);
+  const [tabatas] = useState(1);
+  const [cycles] = useState(8);
+  const [generalMode] = useState(0); // it could be stopped (0), on-going (1), paused (2)
+
+
 
   return (
     <FreeTabataContext.Provider
