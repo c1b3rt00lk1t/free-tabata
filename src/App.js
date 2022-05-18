775f07dc5bf0c4cb18df51bfd2ce449741023704
c1b3rt00lk1t <c1b3rt00lk1t@protonmail.com>
2022-05-18 13:45:06 +0200

    Set up basic routes and pages

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import { FreeTabataProvider } from "./context/FreeTabataContext";
 import CountdownButton from "./components/CountdownButton";
 import TabataCycleBtn from "./components/TabataCycleButton";
@@ -9,22 +10,51 @@ import About from "./components/About";
 function App() {
   return (
     <FreeTabataProvider>
-      <div className={"verticalContainer margintop-5vh"}>
-        <div className={"verticalContainer"}>
-          <CountdownButton type={"prepare"} />
-          <CountdownButton type={"work"} />
-          <CountdownButton type={"rest"} />
+      <Router>
+        <div className={"verticalContainer margintop-5vh"}>
+          <Routes>          
+            <Route exact path='/' element = {
+              <>
+                  <div className={"verticalContainer"}>
+                    <CountdownButton type={"prepare"} />
+                    <CountdownButton type={"work"} />
+                    <CountdownButton type={"rest"} />
+                  </div>
+                  <div className="horizontalContainer">
+                    <TabataCycleBtn type={"cycles"} />
+                    <TabataCycleBtn type={"tabatas"} />
+                  </div>
+                  <div className={"horizontalContainer"}>
+                    <Volume />
+                    <About />
+                  </div>
+                  <StartStopButton />
+              </>
+            }></Route>
+            <Route exact path='/prepare' element = {
+              <><div style={{color:'white'}}>Prepare...</div></>
+            }></Route>
+            <Route exact path='/work' element = {
+              <><div style={{color:'white'}}>Work...</div></>
+            }></Route>
+            <Route exact path='/rest' element = {
+              <><div style={{color:'white'}}>Work...</div></>
+            }></Route>
+            <Route exact path='/cycles' element = {
+              <><div style={{color:'white'}}>Cycles...</div></>
+            }></Route>
+            <Route exact path='/tabatas' element = {
+              <><div style={{color:'white'}}>Tabatas...</div></>
+            }></Route>
+            <Route exact path='/about' element = {
+              <><div style={{color:'white'}}>About...</div></>
+            }></Route>
+            <Route exact path='/ongoing' element = {
+              <><div style={{color:'white'}}>Ongoing...</div></>
+            }></Route>
+          </Routes>  
         </div>
-        <div className="horizontalContainer">
-          <TabataCycleBtn type={"cycles"} />
-          <TabataCycleBtn type={"tabatas"} />
-        </div>
-        <div className={"horizontalContainer"}>
-          <Volume />
-          <About />
-        </div>
-        <StartStopButton />
-      </div>
+      </Router>
     </FreeTabataProvider>
   );
 }
