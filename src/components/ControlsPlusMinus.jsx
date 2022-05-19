64710fd55aad5a3609ef45401ec10f9d965071b5
c1b3rt00lk1t <c1b3rt00lk1t@protonmail.com>
2022-05-20 01:17:11 +0200

    Tabatas and cycles cannot go under 1, and Work, Prepare, Rest cannot go under 0.

--- src/components/ControlsPlusMinus.jsx
+++ src/components/ControlsPlusMinus.jsx
@@ -5,7 +5,16 @@ import FreeTabataContext from "../context/FreeTabataContext";
 const ControlsPlusMinus = ({ type, sign }) => {
 
   const context = useContext(FreeTabataContext);
-  const handleClick = context[`handleSet${type}`]((context[type] + +sign) <= 0 ? 0 : context[type] + +sign);
+  const result = context[type]+ +sign;
+console.log(type)
+  const number = () => {
+    if (type === 'tabatas' || type === 'cycles'){
+      return result <= 1 ? 1 : result;
+    } else {
+      return result <= 0 ? 0 : result;
+    }
+  }
+  const handleClick = context[`handleSet${type}`](number());
   
   
   return ( <div className='controlPlusMinusBtn' onClick={handleClick}>{sign[0]}</div> );
