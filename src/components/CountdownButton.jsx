222207db318a66953a6813c37161cc128eadb3f2
c1b3rt00lk1t <c1b3rt00lk1t@protonmail.com>
2022-05-19 07:45:14 +0200

    Added navigation to prepare/work/rest pages in case of onClick.

--- src/components/CountdownButton.jsx
+++ src/components/CountdownButton.jsx
@@ -1,12 +1,21 @@
 import React, { useContext } from "react";
 import FreeTabataContext from "../context/FreeTabataContext";
+import { useNavigate } from 'react-router-dom'
 
 
 const CountdownButton = ({ type }) => {
+  // Getting the type from the context
   const context = useContext(FreeTabataContext);
   const countdown = context[type];
+
+  // Preparing to navigate in case of a click
+  const navigate = useNavigate();
+  const handleSet = () => {
+    navigate(`/${type}`)
+  };
+
   return (
-    <div className={`${type} countdownBtn `}>
+    <div onClick={handleSet} className={`${type} countdownBtn `}>
         <div className="countdownBtnItems">{type.toUpperCase()}</div>
         <div className="countdownBtnItems">{`${String(Math.floor(countdown/60)).padStart(2,'0')}:${String(countdown%60).padStart(2,'0')}`}</div>
     </div>
