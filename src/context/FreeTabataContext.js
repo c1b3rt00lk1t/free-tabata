ccbc786529ba2965ddda4428ae00af384269fee0
c1b3rt00lk1t <c1b3rt00lk1t@protonmail.com>
2022-05-19 07:06:56 +0200

    Starting from a default set up, it go all through the prepare-(work-rest)*cycles*tabatas until the end, and then stops.

--- src/context/FreeTabataContext.js
+++ src/context/FreeTabataContext.js
@@ -11,35 +11,41 @@ export const FreeTabataProvider = ({ children }) => {
   // const [generalMode, setGeneralMode] = useState(0); // it could be stopped (0), on-going (1), paused (2)
   // const [cycleMode, setCycleMode] = useState("none"); // it could be none, prepare, work, rest
 
-  const [prepare, setPrepare] = useState(5);
-  const [work, setWork] = useState(5);
-  const [rest, setRest] = useState(5);
-  const [tabatas] = useState(1);
-  const [cycles] = useState(8);
+  // default values for testing
+  const NR_TIME = 2;
+  const NR_TBT = 2;
+
+  const [prepare, setPrepare] = useState(NR_TIME);
+  const [work, setWork] = useState(NR_TIME);
+  const [rest, setRest] = useState(NR_TIME);
+  const [tabatas, setTabatas] = useState(NR_TBT);
+  const [cycles, setCycles] = useState(NR_TBT);
   const [generalMode, setGeneralMode] = useState(false); // it could be stopped (false), on-going (true)
   const [pauseMode, setPauseMode] = useState(false); // it could be paused (false), on-going (true)
   const [timer, setTimer] = useState();
 
   const handleStartStop = () => {
     // When the Start/Stop button is clicked and the current mode is working, it stops the timer and resets
-    if(generalMode){
+    if (generalMode) {
       clearTimeout(timer);
-      setPrepare(5);
-      setWork(5);
-      setRest(5);
+      setPrepare(NR_TIME);
+      setWork(NR_TIME);
+      setRest(NR_TIME);
+      setCycles(NR_TBT);
+      setTabatas(NR_TBT);
       setPauseMode(false);
-   }
-   // In any case, the mode is toggled
-   setGeneralMode(!generalMode);
+    }
+    // In any case, the mode is toggled
+    setGeneralMode(!generalMode);
   };
 
   const handleStartPause = () => {
     // When the Start/Pause button is clicked and the current mode is working, it stops the timer but does not reset
-    if(generalMode && pauseMode){
+    if (generalMode && pauseMode) {
       clearTimeout(timer);
-   }
-   // If the generalMode is active, the pause mode is toggled
-   generalMode && setPauseMode(!pauseMode);
+    }
+    // If the generalMode is active, the pause mode is toggled
+    generalMode && setPauseMode(!pauseMode);
   };
 
   useEffect(() => {
@@ -54,7 +60,7 @@ export const FreeTabataProvider = ({ children }) => {
         setTimer(interval);
       }
     };
-    // In this first approach, the work calls the rest when it ends 
+    // In this first approach, the work calls the rest when it ends
     const workCountDown = () => {
       if (work === 0) {
         restCountDown();
@@ -66,11 +72,23 @@ export const FreeTabataProvider = ({ children }) => {
       }
     };
 
-    // In this first approach, the work calls the rest when it ends 
+    // In this first approach, the work calls the rest when it ends
     const restCountDown = () => {
       if (rest === 0) {
-        setWork(5);
-        setRest(5);
+        // While there are active cycles, the work-rest timers are reset and the cycle count is decreased
+        if (cycles > 1) {
+          setWork(NR_TIME);
+          setRest(NR_TIME);
+          setCycles(cycles - 1);
+        } else if (cycles === 1 && tabatas > 1) {
+          setCycles(NR_TBT);
+          setWork(NR_TIME);
+          setRest(NR_TIME);
+          setTabatas(tabatas - 1);
+        } else if (cycles === 1 && tabatas === 1) {
+          setCycles(0);
+          setTabatas(0)
+        }
       } else {
         const interval = setTimeout(() => {
           setRest(rest - 1);
@@ -83,8 +101,7 @@ export const FreeTabataProvider = ({ children }) => {
     if (generalMode && !pauseMode) {
       prepareCountDown();
     }
-  }, [generalMode, prepare, work, rest, pauseMode]);
-
+  }, [generalMode, prepare, work, rest, pauseMode, cycles, tabatas]);
 
   return (
     <FreeTabataContext.Provider
