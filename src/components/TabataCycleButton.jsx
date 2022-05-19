d9473dfcf198687a2e3a7f126b5e186bf3f9ab55
c1b3rt00lk1t <c1b3rt00lk1t@protonmail.com>
2022-05-19 08:56:17 +0200

    Added navigation to Tabata/Cycle button

--- src/components/TabataCycleButton.jsx
+++ src/components/TabataCycleButton.jsx
@@ -1,12 +1,19 @@
 import React, {useContext} from 'react'
 import FreeTabataContext from "../context/FreeTabataContext";
+import { useNavigate } from 'react-router-dom'
 
 const TabataCycleButton = ({type}) => {
   const context = useContext(FreeTabataContext);
   const number = context[type];
 
+  // Preparing to navigate in case of a click
+  const navigate = useNavigate();
+  const handleNavigate = () => {
+    navigate(`/${type}`)
+  };
+
   return (
-    <div className={`${type} `}>
+    <div onClick={handleNavigate} className={`${type} `}>
         <div className="tabataCycleNumber">{`${String(number).padStart(2,'0')}`}</div>
         <div className="">{type.toUpperCase()}</div>
     </div>
