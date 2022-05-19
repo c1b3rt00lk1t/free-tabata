b39bde4abeebfa2ca8b4be6ffd7058ebf023a967
c1b3rt00lk1t <c1b3rt00lk1t@protonmail.com>
2022-05-19 07:53:00 +0200

    Main path corrected to /

--- src/components/DoneButton.jsx
+++ src/components/DoneButton.jsx
@@ -5,7 +5,7 @@ const DoneButton = () => {
   // Preparing to navigate back to Main page in case of a click
   const navigate = useNavigate();
   const handleNavigate = () => {
-    navigate(`/main`)
+    navigate('/')
   }
 
 
