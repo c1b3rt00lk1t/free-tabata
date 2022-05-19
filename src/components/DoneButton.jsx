2678b9fbdf63195bcd079016bd4fb7a9266cf833
c1b3rt00lk1t <c1b3rt00lk1t@protonmail.com>
2022-05-19 07:51:20 +0200

    First version of Done button created.

---
+++ src/components/DoneButton.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { useNavigate } from 'react-router-dom'
+
+const DoneButton = () => {
+  // Preparing to navigate back to Main page in case of a click
+  const navigate = useNavigate();
+  const handleNavigate = () => {
+    navigate(`/main`)
+  }
+
+
+  return (
+    <div onClick={handleNavigate} className='startStopBtn'>Done</div>
+  )
+}
+
+export default DoneButton
