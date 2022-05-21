5b44489718afe6205d2a1dce4c536bf9886a414d
c1b3rt00lk1t <c1b3rt00lk1t@protonmail.com>
2022-05-21 21:58:06 +0200

    Enabled the About button and the About page with minimal content and the Done button. Make it responsive.

--- src/components/AboutButton.jsx
+++ src/components/AboutButton.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import React, {useContext} from 'react'
 import {BsFillQuestionCircleFill} from 'react-icons/bs'
+import { useNavigate } from 'react-router-dom'
+import FreeTabataContext from '../context/FreeTabataContext';
 
 const AboutButton = () => {
+  // Getting the type from the context
+  const {generalMode} = useContext(FreeTabataContext);
+  // Preparing to navigate in case of a click
+  const navigate = useNavigate();
+  const handleNavigate = () => {
+    !generalMode && navigate(`/about`)
+  };
+
+
+
   return (
-    <div className="misc">
+    <div onClick={handleNavigate} className="misc">
     <BsFillQuestionCircleFill color='grey' size={33}/>
   </div>
   )
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,9 +1,30 @@
-import React from 'react'
+import React from "react";
+import DoneButton from "../components/DoneButton";
 
 const About = () => {
   return (
-    <div style={{color:'white'}}>About...</div>
-  )
-}
+    <>
+      <div className="aboutContainer">
+        <h1>
+          <b>Tabata Free</b> - 1.0.0
+        </h1>
+        <h2>
+          <b>Offline mode</b>: install this app as a desktop app or a mobile app
+          and enjoy the training offline!
+        </h2>
+        <p>
+          On iPhone Safari the sound will not play because of
+          this <em>issue</em>: https://developer.apple.com/forums/thread/126342
+        </p>
+      </div>
+      <DoneButton />
+      <div className="developed">
+        <p>Developed by c1b3rt00lk1t</p>
+        <p>github.com/c1b3rt00lk1t/free-tabata/</p>
+        
+      </div>
+    </>
+  );
+};
 
-export default About
+export default About;
