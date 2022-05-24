a1fd49a9b29e01c8c794a00f2eea07bf77713947
c1b3rt00lk1t <c1b3rt00lk1t@protonmail.com>
2022-05-24 18:54:39 +0200

    Added a flickerOnOff property in context (pending to be actually included as a state in the real context).

--- src/components/DisplayCountdown.jsx
+++ src/components/DisplayCountdown.jsx
@@ -8,9 +8,10 @@ const DisplayCountdown = ({type}) => {
   const context = useContext(FreeTabataContext);
   const countdown = context[type].current;
   const generalMode = context['generalMode'];
+  const flickerOnOff = context.flickerOnOff || true;
 
   return (
-    <div className={`${type} display nonSelectable ${(type === 'work' && generalMode) && 'animate-flicker'}`}>
+    <div className={`${type} display nonSelectable ${(type === 'work' && generalMode && flickerOnOff) && 'animate-flicker'}`}>
         <div className=''>{type.toUpperCase()}</div>
         <div className={`countdownDisplayItems `}>{`${String(Math.floor(countdown/60)).padStart(2,'0')}:${String(countdown%60).padStart(2,'0')}`}</div>
     </div>
